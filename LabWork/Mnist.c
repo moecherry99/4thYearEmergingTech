458134088da2ce74f7603a8d90a5948996dfdec3
Alex Cherry <moecherry2@gmail.com>
2019-10-22 22:00:27 +0100

    Update Mnist.c

--- LabWork/Mnist.c
+++ LabWork/Mnist.c
@@ -3,11 +3,16 @@
 int main(int argc, char (*argv[])){
 
     FILE *f = fopen("t10k-images.idx3-ubyte", "rb");
-    int b;
+    int b = 0;
+
+    printf("No of bytes in an int : %d\n", sizeof(b));
+
     for (int i = 0; i < 20; i++){
         fread(&b, 1, 1, f);
-        printf("%02x", b);
+        printf("%02x\n", b);
 
     }
+    printf("\n");
+    
     return 0;
 }
